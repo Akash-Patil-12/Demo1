842cf7a664779fe16461db7f5b0439528996ab2a
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-19 09:54:34 +0530

    [Akash] add: Addition is done

---
+++ MathOperation/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperation
+{
+    public class Add
+    {
+        public void add(int a,int b)
+        {
+            Console.WriteLine("Addition is :" + (a + b));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add obj= new Add();
+            obj.add(2, 5);
+
+
+        }
+    }
+}
